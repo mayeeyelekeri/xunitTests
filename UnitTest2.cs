9e83b2c1743aa9bfeb1746085caa2833d8ad54fa
MM <yelekeri@gmail.com>
2026-02-07 09:08:16 +0000

    init

---
+++ UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿namespace xunitTests;
+
+public class UnitTest2
+{
+        [Theory]
+	[InlineData(3)]
+	[InlineData(5)]
+	[InlineData(6)]
+	public void MyFirstTheory(int value)
+	{
+	    Assert.True(IsOdd(value));
+	}
+
+	bool IsOdd(int value)
+	{
+	    return value % 2 == 1;
+	}
+
+}
